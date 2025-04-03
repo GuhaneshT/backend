5cbef88c7d413c1f83b1ce6ba92121ca384f6e4a
Guhanesh22015 <cb.en.u4cse22015@cb.students.amrita.edu>
2025-04-03 22:03:59 +0530

    nammathana??

--- main.py
+++ main.py
@@ -843,3 +843,4 @@ async def my_submissions(user: dict = Depends(verify_token)):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Render sets PORT dynamically
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+    
